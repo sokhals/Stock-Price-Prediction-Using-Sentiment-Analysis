f9e6e597bb6e3b12be6b075a7e15fdd1a7fa446c
priyankapatil811 <Priyanka@Priyankas-MBP.cable.rcn.com>
2016-04-06 12:18:23 -0400

    sentiment analysis

---
+++ src/IOtweets/Utility.java
@@ -0,0 +1,32 @@
+package IOtweets;
+
+import weka.core.Instances;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Utility {
+    public static BufferedReader readFile(String filename){
+        BufferedReader inputReader=null;
+        try{
+            inputReader = new BufferedReader(new FileReader(filename));
+        }
+        catch(FileNotFoundException ex){
+            System.err.println("File not found: "+filename);
+        }
+        return inputReader;
+    }
+
+    public static String getClass(Instances data, double[] distribution) {
+        double maxProb = distribution[0];
+        int maxIndex = 0;
+        for(int j=0; j<distribution.length;j++){
+            if(distribution[j] > maxProb) {
+                maxProb = distribution[j];
+                maxIndex = j;
+            }
+        }
+        return data.classAttribute().value(maxIndex);
+    }
+}
