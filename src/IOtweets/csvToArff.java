b1a87ba1aeee4b31eb748ef25d03ce8261963636
priyankapatil811 <Priyanka@Priyankas-MacBook-Pro.local>
2016-04-25 17:45:13 -0400

    changes in csv to arff file

--- src/IOtweets/csvToArff.java
+++ src/IOtweets/csvToArff.java
@@ -1,8 +1,7 @@
 package IOtweets;
-
-
-
 import com.opencsv.CSVParser;
+
+import buildModels.BuildPricePredictionModel;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,11 +10,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-public class csvToArff {
+public class CsvToArff {
 
 	/**
-	 *
+	 * @author Priyanka Patil
 	 * @param fileName
 	 * @throws IOException
 	 */
@@ -42,6 +40,98 @@ public class csvToArff {
 			bw.close();
 			
 		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void CSVToArffForPredictionTrain(String fileName) throws IOException
+	{
+		// load CSV
+		CSVParser parser=new CSVParser();
+		String line="";
+		try{
+			BufferedReader bf=new BufferedReader(new FileReader(new File(fileName)));
+			BufferedWriter bw=new BufferedWriter(new FileWriter(new File(fileName.substring(0,fileName.indexOf(".csv"))+".arff")));
+			bw.write("@relation prediction\n");
+			bw.write("@attribute open numeric\n");
+			bw.write("@attribute high numeric\n");
+			bw.write("@attribute low numeric\n");
+			bw.write("@attribute close numeric\n");
+			//bw.write("@attribute adjClose numeric\n");
+			bw.write("@attribute sentiment {1,-1}\n");
+			bw.write("@attribute prediction_class {1,-1}\n\n");
+			bw.write("@data\n\n");
+			bf.readLine();
+			bf.readLine();
+			while((line=bf.readLine())!=null){
+				String array[]=parser.parseLine(line);
+				if(line.endsWith("0"))
+					continue;
+				bw.write(array[2]+","+array[3]+","+array[4]+","+array[5]+","+array[9]+","+array[10]+"\n");
+			}
+			bf.close();
+			bw.flush();
+			bw.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println(line);
+		}
+	}
+	
+	public static void CSVToArffForPredictionTest(String fileName) throws IOException
+	{
+		// load CSV
+		CSVParser parser=new CSVParser();
+		try{
+			BufferedReader bf=new BufferedReader(new FileReader(new File(fileName)));
+			BufferedWriter bw=new BufferedWriter(new FileWriter(new File(fileName.substring(0,fileName.indexOf(".csv"))+".arff")));
+			bw.write("@relation prediction\n");
+			bw.write("@attribute open numeric\n");
+			bw.write("@attribute high numeric\n");
+			bw.write("@attribute low numeric\n");
+			bw.write("@attribute close numeric\n");
+			//bw.write("@attribute adjClose numeric\n");
+			bw.write("@attribute sentiment {1,-1}\n");
+			bw.write("@attribute prediction_class {1,-1}\n\n");
+			bw.write("@data\n\n");
+			String line="";
+			bf.readLine();
+			bf.readLine();
+			while((line=bf.readLine())!=null){
+				String array[]=parser.parseLine(line);
+				if(line.endsWith("0"))
+					continue;
+				bw.write(array[2]+","+array[3]+","+array[4]+","+array[5]+","+array[8]+","+array[9]+"\n");
+			}
+			bf.close();
+			bw.flush();
+			bw.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		try {
+			CSVToArffForPredictionTrain("outputFiles/StockPrediction_Train_Final.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			CSVToArffForPredictionTest("outputFiles/StockPrediction_Test.csv");
+			try {
+				BuildPricePredictionModel bp = new BuildPricePredictionModel();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
